4723eea0d12cf9925dfc37f6ea77c96ba4627b7d
Wellington Costa <wcosta.ale@gmail.com>
2023-11-27 17:35:51 -0300

    Componente contratos com consulta json serve com rel entre tabelas

--- src/components/vendas/Contratos.vue
+++ src/components/vendas/Contratos.vue
@@ -1,3 +1,36 @@
 <template>
-  <h5>Contratos</h5>
+  <div>
+    <h5>Contratos</h5>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">LEAD</th>
+          <th scope="col">SERVICO</th>
+          <th scope="col">DATA INÍCIO</th>
+          <th scope="col">DATA FIM</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(d, i) in dados" :key="i">
+          <td>{{ d.id }}</td>
+          <td>{{ d.lead.nome }}</td>
+          <td>{{ d.servico.servico }}</td>
+          <td>{{ d.data_inicio }}</td>
+          <td>{{ d.data_fim }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+<script>
+import ApiMixin from '@/mixins/ApiMixin'
+
+export default {
+  name: "Contratos",
+  mixins: [ApiMixin],
+  created() {
+    this.getDadosApi('http://localhost:3000/contratos?_expand=lead&_expand=servico');
+  }
+}
+</script>

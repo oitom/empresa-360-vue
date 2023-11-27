7429b20a0c41d369a162806444b4d5c910746c6e
Wellington Costa <wcosta.ale@gmail.com>
2023-11-27 12:49:27 -0300

    Add views da tela home com navegação no menu lateral

--- src/router.js
+++ src/router.js
@@ -1,11 +1,31 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/vews/Home.vue'
 import Login from '@/vews/Login.vue'
+import Site from '@/vews/Site.vue'
+import Vendas from '@/components/vendas/Vendas.vue'
+import Servicos from '@/components/servicos/Servicos.vue'
+import Leads from '@/components/vendas/Leads.vue'
+import Contratos from '@/components/vendas/Contratos.vue'
+import Dashboard from '@/components/dashboard/Dashboard.vue'
 
 const routes =[
   {
+    path: '/',
+    component: Site
+  },
+  {
     path: '/home',
-    component: Home
+    component: Home,
+    children: [
+      { path: 'vendas', component: Vendas, children: // /home/vendas
+        [
+          { path: 'leads', component: Leads }, // /home/vendas/leads
+          { path: 'contratos', component: Contratos }, // /home/vendas/contratos
+        ] 
+      },
+      { path: 'servicos', component: Servicos}, // /home/servicos
+      { path: 'dashboard', component: Dashboard}, // /home/dashboard
+    ]
   },
   {
     path: '/login',
@@ -14,7 +34,7 @@ const routes =[
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: routes
 })
 
--- src/vews/Home.vue
+++ src/vews/Home.vue
@@ -1,12 +1,74 @@
 <template>
-    <div>home</div>
+  <div>
+    <nav class="navbar navbar-light bg-light menu-superior">
+      <div class="container">
+        <router-link class="navbar-brand" to="/home">Empresa 360</router-link>
+        <div class="navbar-nav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Sair</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+      <div class="d-flex">
+          <div class="list-group menu-esquerda">
+              <router-link class="list-group-item list-group-item-action" to="/home/dashboard" exact-active-class="active">
+                <i class="bi bi-speedometer2"></i> Dashboard
+              </router-link>
+              
+              <router-link class="list-group-item list-group-item-action" to="/home/vendas" exact-active-class="active">
+                <i class="bi bi-cart"></i> Vendas
+              </router-link>
+
+                <router-link class="list-group-item list-group-item-action" to="/home/vendas/leads" exact-active-class="active">
+                  <i class="bi bi-caret-right"></i> Leads
+                </router-link>
+                
+                <router-link class="list-group-item list-group-item-action" to="/home/vendas/contratos" exact-active-class="active">
+                  <i class="bi bi-caret-right"></i> Contratos
+                </router-link>
+
+              <router-link class="list-group-item list-group-item-action" to="/home/servicos" exact-active-class="active">
+                <i class="bi bi-card-checklist"></i> Serviços
+              </router-link>
+          </div>
+
+          <div class="w-100">
+              <nav aria-label="breadcrumb" class="pt-1 ps-3">
+                  <ol class="breadcrumb">
+                      <li class="breadcrumb-item muted-text" aria-current="page">Dashboard</li>
+                  </ol>
+              </nav>
+
+              <div class="container-fluid">
+                  <router-view></router-view>
+              </div>
+
+          </div>
+      </div>
+  </div>
 </template>
-<script>
 
-export default {
-    name: "Home"
-}
-</script>
+
 <style scoped>
+.menu-superior {
+    border-bottom: solid 1px #ccc;
+}
 
+.menu-esquerda {
+    border-right: solid 1px #ccc;
+    width: 250px;
+}
+.list-group-item {
+    border-top: none;
+    border-right: none;
+}
+.active {
+  color: white;
+  background-color: #0d6efd;
+  border-color: #0d6efd;
+}
 </style>
